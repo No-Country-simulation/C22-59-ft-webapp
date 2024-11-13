f9e8d5edae12ba6329b8bb20fa198feeded22af8
Cristian Machuca <cmachuca32@gmail.com>
2024-11-13 19:29:32 +0200

    Uploading Project No Country: Clinic Managment.

---
+++ backend/src/app.ts
@@ -0,0 +1,19 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import path from "path";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "static")));
+
+const PORT = Number(process.env.PORT) || 4000;
+
+app.get("/", (req, res) => {
+  res.sendFile("index.html");
+});
+
+app.listen(PORT, (): void => {
+  console.log(`Server running on port ${PORT}`);
+});
