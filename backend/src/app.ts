1168fe3444121a59dacf2b232cd4e5babbf21889
J4F3ET <jslopezz@udistrital.edu.co>
2024-11-18 11:07:02 -0500

    feat: :sparkles: Swagger implementation and created example file of environment variables

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import path from "path";
+import swaggerDocs from "./docs/swagger";
 
 const app = express();
 app.use(cors());
@@ -9,11 +10,26 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "static")));
 
 const PORT = Number(process.env.PORT) || 4000;
-
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     description: Root path
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: Successful operation
+ *         content:
+ *           text/html:
+ *             schema:
+ *               type: string
+ */
 app.get("/", (req, res) => {
   res.sendFile("index.html");
 });
 
 app.listen(PORT, (): void => {
+  swaggerDocs(app);
   console.log(`Server running on port ${PORT}`);
 });
