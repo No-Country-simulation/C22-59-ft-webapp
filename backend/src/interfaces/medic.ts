a653f5a6b32c38c1ceec730d8dc59f7be3b7e477
JoacooReyes <joacooreyes@gmail.com>
2024-11-26 17:26:57 -0300

    adding medic model

---
+++ backend/src/interfaces/medic.ts
@@ -0,0 +1,14 @@
+export interface IMedic {
+    birthday: string;
+    name: string;
+    surname: string;
+    nationalId: string;
+    email: string;
+    password: string;
+    telephone: string;
+    optionalTelephone?: string;
+    blood: string;
+    gender: string;
+    appointment: string;
+  }
+  
