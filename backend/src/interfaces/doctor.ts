1cfee6a67005f12bd104831299d258cdaac34436
J4F3ET <jslopezz@udistrital.edu.co>
2024-12-11 11:16:56 -0500

    feat: name change medic to doctor

---
+++ backend/src/interfaces/doctor.ts
@@ -0,0 +1,13 @@
+export interface Idoctor {
+	birthday: string;
+	name: string;
+	surname: string;
+	nationalId: string;
+	email: string;
+	password: string;
+	telephone: string;
+	optionalTelephone?: string;
+	blood: string;
+	gender: string;
+	appointment: string;
+}
